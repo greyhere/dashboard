7f8829315be1486aca3c6a8732efc76e7c9bb4bf
greyhere <57859708+greyhere@users.noreply.github.com>
2023-03-25 22:18:51 +0530

    backup

--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,9 @@
-const Home = () => <div>Dashboard</div>;
+'use client';
+
+const Home = () => (
+  <div className='bg-primary dark:bg-secondary'>
+    <p className='text-textColor-primary'>Light</p>
+  </div>
+);
 
 export default Home;
--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,21 @@
 module.exports = {
   content: ['./app/**/*.{js,ts,jsx,tsx}', './components/**/*.{js,ts,jsx,tsx}'],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        primary: 'var(--primary)',
+        secondary: 'var(--secondary)',
+        hover: 'var(--hover)',
+        warning: 'var(--warning)',
+        success: 'var(--success)',
+        green: 'var(--green)',
+        brown: 'var(--brown)',
+        textColor: {
+          primary: 'var(--text-primary)',
+          secondary: 'var(--text-secondary)',
+        },
+      },
+    },
   },
   plugins: [],
 };
